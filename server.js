be0713286379387c9421f1a05d0d8be263906b8f
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-02-19 12:05:35 +0530

    updated server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://sanskrutitechnoschool.com",
+      "https://paradiseems.co.in",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
